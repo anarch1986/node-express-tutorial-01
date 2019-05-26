77e58fd7460d31b14b3c5020c62589465f9c6c95
Tamas Varga <varga.tamas19860808@gmail.com>
2019-05-26 18:24:53 +0200

    Styling added

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin')
-
 const shopRoutes = require('./routes/shop')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
 
