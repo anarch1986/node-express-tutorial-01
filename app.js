606d476c2a22dac07e1f78a42edc08024d88bb58
Tamas Varga <varga.tamas19860808@gmail.com>
2019-05-27 21:58:41 +0200

    new styling added

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const shopRoutes = require('./routes/shop')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 
 });
 
