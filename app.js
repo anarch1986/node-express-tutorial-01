c5b306d85384c1be560a940b5288b2c26d55ed89
Tamas Varga <varga.tamas19860808@gmail.com>
2019-05-29 22:54:26 +0200

    Handlebars templating added

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views')
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {layout: false, pageTitle: 'Page Not Found'});
 
 });
 
