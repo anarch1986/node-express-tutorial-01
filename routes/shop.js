c6e4dabe0f4075511cf6d29ed74420056ce41a42
Varga Tamás <varga.tamas19860808@gmail.com>
2019-06-01 21:19:35 +0200

    ejs templating added

--- routes/shop.js
+++ routes/shop.js
@@ -7,6 +7,7 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
+    console.log(products);
     res.render('shop', {
         prods: products, 
         pageTitle: 'Shop', 
